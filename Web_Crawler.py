b57b6fc154a6fa9b0d86533125a9296cecc55d3c
Chaoyue <hechaoyue0307@gmail.com>
2016-08-26 17:09:57 +0800

    Defined a function to complete the page html extraction

--- Web_Crawler.py
+++ Web_Crawler.py
@@ -1,8 +1,13 @@
 import urllib2
 
+def get_page_html(page_url):
+    response = urllib2.urlopen(page_url)
+    html = response.read()
+    return html
 
-response = urllib2.urlopen('https://web.whatsapp.com/')
-html = response.read()
-print html
+
+url = "https://www.google.com.sg"
+page = get_page_html(url)
+print page
 
   
